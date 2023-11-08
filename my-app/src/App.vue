45a2da15ba750ea1927cbabd0dfc15b71da33998
JsquareNG <jingjie.ng.2022@scis.smu.edu.sg>
2023-11-08 10:51:29 +0800

    change

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -95,7 +95,7 @@
   </div>
   <router-view/>
   <footer id="footer" class="bg-light text-center text-lg-start">
-        <div class="container p-4">
+        <div class="container">
             <div class="row">
                 <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
                     <h5 class="text-uppercase">Featured</h5>
